583a35e680bc02d113fb626a6b2633abf4f38d5e
UO294013 <uo294013@uniovi.es>
2025-10-30 12:22:05 +0100

    Acabada sesión 6

---
+++ js/cronometro.js
@@ -0,0 +1,51 @@
+class Cronometro {
+
+    constructor() {
+        this.tiempo = 0;
+    }
+
+    arrancar() {
+        if (this.corriendo == null && this.tiempo == 0) {
+            try {
+                this.inicio = Temporal.now();
+            } catch (e) {
+                this.inicio = Date.now(); // Si Temporal no está disponible
+            }
+            // Utilizar bind() para asegurar que 'this' se refiere a la instancia de Cronometro
+            this.corriendo = setInterval(this.actualizar.bind(this), 100); // 100 ms -> actualizar cada 0.1 segundos
+        }
+    }
+
+    actualizar() {
+        try {
+            this.tiempo = Number(Temporal.now().instant().since(this.inicio, { smallestUnit: 'milliseconds' }).total('milliseconds'));
+        } catch (e) {
+            this.tiempo = Date.now() - this.inicio; // Si Temporal no está disponible
+        }
+        this.mostrar();
+    }
+
+    mostrar() {
+        this.minutos = parseInt(this.tiempo / 60000);
+        this.segundos = parseInt((this.tiempo % 60000) / 1000);
+        this.decimas = parseInt((this.tiempo % 1000) / 100);
+
+        const mm = String(this.minutos).padStart(2, '0');
+        const ss = String(this.segundos).padStart(2, '0');
+        const d = this.decimas;
+        const p = document.querySelector('main p:nth-of-type(1)');
+        p.textContent = `${mm}:${ss}.${d}`;
+    }
+
+    parar() {
+        clearInterval(this.corriendo);
+        this.corriendo = null;
+    }
+
+    reiniciar() {
+        clearInterval(this.corriendo);
+        this.corriendo = null;
+        this.tiempo = 0;
+        this.mostrar();
+    }
+}
--- js/memoria.js
+++ js/memoria.js
@@ -1,10 +1,78 @@
 class Memoria {
 
     constructor() {
-        
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+        this.barajarCartas();
+        this.tablero_bloqueado = false;
+        this.cronometro = new Cronometro();
+        this.cronometro.arrancar();
     }
 
     voltearCarta(carta) {
-        carta.setAttribute("data-estado", "revelada");
+        if (carta.getAttribute("data-estado") !== "volteada" && carta.getAttribute("data-estado") !== "revelada" && !this.tablero_bloqueado) {
+            carta.setAttribute("data-estado", "volteada");
+            if (this.primera_carta === null) {
+                this.primera_carta = carta;
+            } else {
+                this.segunda_carta = carta;
+                this.comprobarPareja();
+            }
+        }
+    }
+
+    barajarCartas() {
+        const contenedor = document.querySelector('main');
+        let cartas = Array.from(contenedor.querySelectorAll('article'));
+        for (let i = cartas.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cartas[i], cartas[j]] = [cartas[j], cartas[i]];
+        }
+        cartas.forEach(carta => contenedor.appendChild(carta));
+    }
+
+    reiniciarAtributos() {
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+    }
+
+    deshabilitarCartas() {
+        this.primera_carta.setAttribute("data-estado", "revelada");
+        this.segunda_carta.setAttribute("data-estado", "revelada");
+        this.reiniciarAtributos();
+        if (this.comprobarJuego()) {
+            this.cronometro.parar();
+        } else {
+            this.tablero_bloqueado = false;
+        }
+    }
+
+    comprobarJuego() {
+        let todas_reveladas = true;
+        const contenedor = document.querySelector('main');
+        let cartas = Array.from(contenedor.querySelectorAll('article'));
+        cartas.forEach(carta => {
+            if (carta.getAttribute("data-estado") !== "revelada") {
+                todas_reveladas = false;
+            } 
+        });
+        return todas_reveladas;
+    }
+
+    cubrirCartas() {
+        this.tablero_bloqueado = true;
+        setTimeout(() => {
+            this.primera_carta.setAttribute("data-estado", "");
+            this.segunda_carta.setAttribute("data-estado", "");
+            this.reiniciarAtributos();
+            this.tablero_bloqueado = false;
+        }, 1500); // Delay de 1.5 segundos antes de cubrir las cartas de nuevo
+    }
+
+    comprobarPareja() {
+        let iguales = this.primera_carta.querySelector('img').getAttribute("alt") == this.segunda_carta.querySelector('img').getAttribute("alt");
+        iguales ? this.deshabilitarCartas() : this.cubrirCartas();
     }
 }
